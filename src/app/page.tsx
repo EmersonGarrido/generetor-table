1828147f8ac17c96ae14e51fb97ab2165dce259a
EmersonGarrido <emersongarrido@hotmail.com.br>
2023-05-02 17:44:36 -0400

    add context by search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,41 @@
 "use client";
+import { useState } from "react";
 
 export default function Home() {
-  const doc: any = document;
+  const [titulo, setTitulo] = useState("");
+  const [colunas, setColunas] = useState([{ label: "", value: "" }]);
+  const [html, setHtml] = useState("");
+
+  function handleTituloChange(event: any) {
+    setTitulo(event.target.value);
+  }
+
+  function handleLabelChange(event: any, index: any) {
+    const newColunas = [...colunas];
+    newColunas[index].label = event.target.value;
+    setColunas(newColunas);
+  }
+
+  function handleValueChange(event: any, index: any) {
+    const newColunas = [...colunas];
+    newColunas[index].value = event.target.value;
+    setColunas(newColunas);
+  }
 
   function gerarTabela() {
-    var titulo = doc.getElementById("titulo").value;
-    var colunas: any = document.getElementsByClassName("coluna");
     var colunaLabels = [];
     var colunaValues = [];
 
     for (var i = 0; i < colunas.length; i++) {
-      colunaLabels.push(colunas[i].getElementsByClassName("label")[0].value);
-      colunaValues.push(colunas[i].getElementsByClassName("value")[0].value);
+      colunaLabels.push(colunas[i].label);
+      colunaValues.push(colunas[i].value);
     }
 
     var tabelaHTML = "<div>\n";
     tabelaHTML +=
-      '<div class="titulo-categoria"><strong>' + titulo + "</strong></div>\n";
+      '<div class="titulo-categoria" style="text-align: center;"><strong>' +
+      titulo +
+      "</strong></div>\n";
     tabelaHTML += '    <table style="width: 100%; font-size: 14pt">\n';
     tabelaHTML += "      <tbody>\n";
 
@@ -35,75 +54,69 @@ export default function Home() {
     tabelaHTML += "    </table>\n";
     tabelaHTML += "</div>";
 
-    doc.getElementById("codigoHTML").value = tabelaHTML;
+    setHtml(tabelaHTML);
   }
 
-  function adicionarColuna() {
-    var novaColuna = document.createElement("div");
-    novaColuna.className =
-      "coluna flex flex-col gap-1 border-b-[0.1rem] pb-5 pt-5 border-black/5";
-
-    var novoLabel = document.createElement("label");
-    novoLabel.innerHTML = "Titulo da Coluna";
-    var novoInputLabel = document.createElement("input");
-    novoInputLabel.type = "text";
-    novoInputLabel.className =
-      "border-[0.1rem] border-[#dadee1] rounded-md p-2 label";
-    var novoValor = document.createElement("label");
-    novoValor.innerHTML = "Valor";
-    var novoInputValor = document.createElement("input");
-    novoInputValor.type = "text";
-    novoInputValor.className =
-      "border-[0.1rem] border-[#dadee1] rounded-md p-2 value";
-
-    novaColuna.appendChild(novoLabel);
-    novaColuna.appendChild(novoInputLabel);
-    novaColuna.appendChild(novoValor);
-    novaColuna.appendChild(novoInputValor);
-
-    doc.getElementById("colunas").appendChild(novaColuna);
+  function addColuna() {
+    setColunas([...colunas, { label: "", value: "" }]);
   }
-
   return (
     <main className="w-full flex flex-col items-center justify-center bg-[#f6f6f8] h-screen">
       <div className="border-[0.1rem] border-[#dadee1] rounded-md bg-white w-full max-w-2xl">
         <div className="text-center p-4">
           <h1 className="text-[#574967]">Construtor de tabela</h1>
         </div>
+
         <div className="flex flex-col gap-2 p-4">
-          <label className="text-[#574967]">Título</label>
+          <label className="text-[#574967]" htmlFor="titulo">
+            Título:
+          </label>
           <input
             className="border-[0.1rem] border-[#dadee1] rounded-md p-2"
             type="text"
             id="titulo"
+            value={titulo}
+            onChange={handleTituloChange}
           />
         </div>
-        <div id="colunas" className="p-4">
-          <div className="coluna flex flex-col gap-1 border-b-[0.1rem] pb-5 pt-5 border-black/5">
-            <label className="text-[#574967]">Titulo da Coluna</label>
-            <input
-              type="text"
-              className="border-[0.1rem] border-[#dadee1] rounded-md p-2 label"
-            />
-            <label className="text-[#574967]">Valor</label>
-            <input
-              type="text"
-              className="border-[0.1rem] border-[#dadee1] rounded-md p-2 value"
-            />
-          </div>
+
+        <div className="colunas p-4 flex flex-col gap-3">
+          {colunas.map((coluna, index) => (
+            <div
+              className="coluna flex items-center justify-between gap-2"
+              key={index}
+            >
+              <label className="text-[#574967]">Coluna</label>
+              <input
+                type="text"
+                className="border-[0.1rem] border-[#dadee1] rounded-md p-2 label w-full"
+                placeholder="Titulo"
+                value={coluna.label}
+                onChange={(event) => handleLabelChange(event, index)}
+              />
+              <input
+                type="text"
+                className="border-[0.1rem] border-[#dadee1] rounded-md p-2 value w-full"
+                placeholder="Valor"
+                value={coluna.value}
+                onChange={(event) => handleValueChange(event, index)}
+              />
+            </div>
+          ))}
         </div>
         <div className="flex items-center justify-between p-4">
           <button
             className="bg-black/75 hover:bg-black text-white rounded-md px-4 py-2"
-            onClick={() => adicionarColuna()}
+            onClick={addColuna}
           >
-            Adicionar Coluna
+            Adicionar coluna
           </button>
+
           <button
             className="bg-orange-600 hover:bg-orange-900 text-white rounded-md px-4 py-2"
-            onClick={() => gerarTabela()}
+            onClick={gerarTabela}
           >
-            Gerar Tabela
+            Gerar tabela
           </button>
         </div>
 
@@ -112,12 +125,12 @@ export default function Home() {
           <textarea
             rows={15}
             className="border-[0.1rem] border-[#dadee1] rounded-md p-2 w-full"
-            id="codigoHTML"
+            value={html}
           ></textarea>
         </div>
       </div>
       <span className="mt-5 font-thin text-sm text-center">
-        Todos os direitos reservados a{" "}
+        Todos os direitos reservados a
         <a className="font-normal" href="https://emersongarrido.com.br">
           Emerson Garrido
         </a>
