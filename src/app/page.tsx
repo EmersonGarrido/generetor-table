59fb190366000961ac0918b9861d6d7783425b2e
EmersonGarrido <emersongarrido@hotmail.com.br>
2023-05-02 17:24:18 -0400

    add context by search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,116 +1,129 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+"use client";
 
 export default function Home() {
+  const doc: any = document;
+
+  function gerarTabela() {
+    var titulo = doc.getElementById("titulo").value;
+    var colunas: any = document.getElementsByClassName("coluna");
+    var colunaLabels = [];
+    var colunaValues = [];
+
+    for (var i = 0; i < colunas.length; i++) {
+      colunaLabels.push(colunas[i].getElementsByClassName("label")[0].value);
+      colunaValues.push(colunas[i].getElementsByClassName("value")[0].value);
+    }
+
+    var tabelaHTML = "<div>\n";
+    tabelaHTML +=
+      '  <h1 style="text-align: center; titulo-categoria">' +
+      titulo +
+      "</h1>\n";
+    tabelaHTML += '    <table style="width: 100%; font-size: 14pt">\n';
+    tabelaHTML += "      <tbody>\n";
+
+    for (var i = 0; i < colunas.length; i++) {
+      var cor = i === 0 ? "#e5e5e5" : "#fff";
+      tabelaHTML +=
+        '        <tr style="background-color: ' +
+        cor +
+        '; color: #6d6d6d; height: 40px;">\n';
+      tabelaHTML += "          <td>" + colunaLabels[i] + "</td>\n";
+      tabelaHTML += "          <td>" + colunaValues[i] + "</td>\n";
+      tabelaHTML += "        </tr>\n";
+    }
+
+    tabelaHTML += "      </tbody>\n";
+    tabelaHTML += "    </table>\n";
+    tabelaHTML += "</div>";
+
+    doc.getElementById("codigoHTML").value = tabelaHTML;
+  }
+
+  function adicionarColuna() {
+    var novaColuna = document.createElement("div");
+    novaColuna.className =
+      "coluna flex flex-col gap-1 border-b-[0.1rem] pb-5 pt-5 border-black/5";
+
+    var novoLabel = document.createElement("label");
+    novoLabel.innerHTML = "Titulo da Coluna";
+    var novoInputLabel = document.createElement("input");
+    novoInputLabel.type = "text";
+    novoInputLabel.className =
+      "border-[0.1rem] border-[#dadee1] rounded-md p-2 label";
+    var novoValor = document.createElement("label");
+    novoValor.innerHTML = "Valor";
+    var novoInputValor = document.createElement("input");
+    novoInputValor.type = "text";
+    novoInputValor.className =
+      "border-[0.1rem] border-[#dadee1] rounded-md p-2 value";
+
+    novaColuna.appendChild(novoLabel);
+    novaColuna.appendChild(novoInputLabel);
+    novaColuna.appendChild(novoValor);
+    novaColuna.appendChild(novoInputValor);
+
+    doc.getElementById("colunas").appendChild(novaColuna);
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <main className="w-full flex flex-col items-center justify-center bg-[#f6f6f8] h-screen">
+      <div className="border-[0.1rem] border-[#dadee1] rounded-md bg-white w-full max-w-2xl">
+        <div className="text-center p-4">
+          <h1 className="text-[#574967]">Construtor de tabela</h1>
+        </div>
+        <div className="flex flex-col gap-2 p-4">
+          <label className="text-[#574967]">Título</label>
+          <input
+            className="border-[0.1rem] border-[#dadee1] rounded-md p-2"
+            type="text"
+            id="titulo"
+          />
+        </div>
+        <div id="colunas" className="p-4">
+          <div className="coluna flex flex-col gap-1 border-b-[0.1rem] pb-5 pt-5 border-black/5">
+            <label className="text-[#574967]">Titulo da Coluna</label>
+            <input
+              type="text"
+              className="border-[0.1rem] border-[#dadee1] rounded-md p-2 label"
             />
-          </a>
+            <label className="text-[#574967]">Valor</label>
+            <input
+              type="text"
+              className="border-[0.1rem] border-[#dadee1] rounded-md p-2 value"
+            />
+          </div>
+        </div>
+        <div className="flex items-center justify-between p-4">
+          <button
+            className="bg-black/75 hover:bg-black text-white rounded-md px-4 py-2"
+            onClick={() => adicionarColuna()}
+          >
+            Adicionar Coluna
+          </button>
+          <button
+            className="bg-orange-600 hover:bg-orange-900 text-white rounded-md px-4 py-2"
+            onClick={() => gerarTabela()}
+          >
+            Gerar Tabela
+          </button>
+        </div>
+
+        <div className="p-4">
+          <h2 className="text-[#574967]">Código HTML Gerado:</h2>
+          <textarea
+            rows={15}
+            className="border-[0.1rem] border-[#dadee1] rounded-md p-2 w-full"
+            id="codigoHTML"
+          ></textarea>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
+      <span className="mt-5 font-thin text-sm text-center">
+        Todos os direitos reservados a{" "}
+        <a className="font-normal" href="https://emersongarrido.com.br">
+          Emerson Garrido
         </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </span>
     </main>
-  )
+  );
 }
